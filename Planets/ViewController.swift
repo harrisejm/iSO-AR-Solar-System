aca334fb13ce4400beb8715c58dcf20c2a0c66cb
e <harrisejm@gmail.com>
2019-09-18 23:36:10 -0700

    add planets Uranus and Neptuna

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -24,17 +24,48 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    var size : Double = 1
+    
+    @IBAction func changeSizeSmall(_ sender: UIButton) {
+        size = 0.7
+        print("H")
+        self.sceneView.scene.rootNode.enumerateChildNodes({ (node, _) in
+            node.removeFromParentNode()
+        })
+        createPlanets()
+    }
+    
+    @IBAction func changeSizeMedium(_ sender: UIButton) {
+        size = 1.0
+        print("H")
+        self.sceneView.scene.rootNode.enumerateChildNodes({ (node, _) in
+            node.removeFromParentNode()
+        })
+        createPlanets()
+    }
+    
+    @IBAction func changeSizeLarge(_ sender: UIButton) {
+        size = 1.5
+        print("H")
+        self.sceneView.scene.rootNode.enumerateChildNodes({ (node, _) in
+            node.removeFromParentNode()
+        })
+        createPlanets()
+    }
+    
+    
     func planetTemplate(_ radiusTemplate: CGFloat, _ diffuseTemplate: String, _ specularTemplate: String?, _ emissionTemplate: String?, _ normalTemplate: String?, _ distanceTemplate: Double, _ durationTemplate: TimeInterval) {
         
         let node = SCNNode()
         node.position = SCNVector3(0,0.5,-2)
         self.sceneView.scene.rootNode.addChildNode(node)
         
-        let planetNode = planet(geometry: SCNSphere(radius: radiusTemplate), diffuse: diffuseTemplate, specular: specularTemplate, emission: emissionTemplate, normal: normalTemplate, position: SCNVector3(distanceTemplate,0,0))
+        let planetNode = planet(geometry: SCNSphere(radius: radiusTemplate*CGFloat(size)), diffuse: diffuseTemplate, specular: specularTemplate, emission: emissionTemplate, normal: normalTemplate, position: SCNVector3(distanceTemplate*size,0,0))
 
         rotatePlanet(node,planetNode,durationTemplate)
         planetPath(distanceTemplate)  //path rings
         attachMoonToEarth(diffuseTemplate,planetNode)
+        attachRingToSaturn(diffuseTemplate,planetNode)
     }
     
     func rotatePlanet(_ node: SCNNode, _ planetNode: SCNNode, _ durationTemplate: TimeInterval){
@@ -47,7 +78,7 @@ class ViewController: UIViewController {
     //path rings
     func planetPath(_ radius: Double){
         let node = SCNNode()
-        node.geometry = SCNTorus(ringRadius: CGFloat(radius), pipeRadius: 0.0005)
+        node.geometry = SCNTorus(ringRadius: CGFloat(radius*size), pipeRadius: 0.0005)
         node.position = SCNVector3(0,0.5,-2)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
@@ -61,47 +92,38 @@ class ViewController: UIViewController {
         }
     }
     
-    func mercury(){
-        planetTemplate(0.04, "mercury", nil, nil, nil, 0.5, 3)
+    func attachRingToSaturn(_ planet: String, _ node: SCNNode) {
+        if planet == "saturn" {
+            node.addChildNode(saturnRing())
+        }
     }
     
-    func venus(){
-        planetTemplate(0.07, "venus surface", nil, "venus atmosphere", nil, 0.8, 5)
-    }
-    
-    func earth(){
-         planetTemplate(0.15, "earth day", "earth specular", "earth clouds", "earth normal", 1.4, 8)
-    }
     
     func moon() -> SCNNode {
-         let moon = planet(geometry: SCNSphere(radius: 0.04), diffuse: "moon", specular: nil, emission: nil, normal: nil, position: SCNVector3(0,0,0.3))
+        let moon = planet(geometry: SCNSphere(radius: CGFloat(0.04*size)), diffuse: "moon", specular: nil, emission: nil, normal: nil, position: SCNVector3(0,0,0.25*size))
         return moon
     }
     
-    func mars(){
-        planetTemplate(0.12, "mars", nil, nil, nil, 2, 13)
+    func saturnRing() -> SCNNode {
+        let saturn = planet(geometry: SCNTube(innerRadius: CGFloat(0.17*size), outerRadius: CGFloat(0.28*size), height: CGFloat(0.003*size)), diffuse: "saturn ring", specular: nil, emission: nil, normal: nil, position: SCNVector3(0,0,0))
+        return saturn
     }
     
-    func jupiter(){
-        planetTemplate(0.20, "jupiter", nil, nil, nil, 3, 15)
-    }
-    
-    
-    
-    
-    func sun(){
-        let sun = planet(geometry: SCNSphere(radius: 0.35), diffuse: "sun diffuse", specular: nil, emission: nil, normal: nil, position: SCNVector3(0,0.5,-2))
-        self.sceneView.scene.rootNode.addChildNode(sun)
+    func createPlanets(){
+        planetTemplate(CGFloat(0.04*size), "mercury", nil, nil, nil, 0.6*size, 3*size)
+        planetTemplate(CGFloat(0.07*size), "venus surface", nil, "venus atmosphere", nil, 0.8*size, 5*size)
+        planetTemplate(CGFloat(0.15*size), "earth day", "earth specular", "earth clouds", "earth normal", 1.4*size, 8*size)
+        planetTemplate(CGFloat(0.12*size), "mars", nil, nil, nil, 2*size, 13*size)
+        planetTemplate(CGFloat(0.20*size), "jupiter", nil, nil, nil, 2.9*size, 15*size)
+        planetTemplate(CGFloat(0.1*size), "saturn", nil, nil, nil, 3.6*size, 18*size)
+        planetTemplate(CGFloat(0.07*size), "uranus", nil, nil, nil, 4.1*size, 23*size)
+        planetTemplate(CGFloat(0.07*size), "neptune", nil, nil, nil, 4.5*size, 30*size)
+        
+        planetTemplate(CGFloat(0.35*size), "sun diffuse", nil, nil, nil, 0, 23*size)
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        
-        sun()
-        mercury()
-        venus()
-        earth()
-        mars()
-        jupiter()
+        createPlanets()
     }
     
     func planet(geometry: SCNGeometry, diffuse: String, specular: String?, emission: String?, normal: String?, position: SCNVector3) -> SCNNode {
